608e20272078a89ccecc503e3801e48cfe9a5425
Mac <tirumala@appincubator.io>
2023-11-27 12:57:09 +0530

    Initial Commit

---
+++ ettemeneTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ettemeneTask
+//
+//  Created by Mac on 27/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
